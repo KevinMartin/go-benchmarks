cf9c3e137cf12255b95c2c4014dd504f1b701d2e
Kevin Jose Martin <kevin@tiliq.com>
2018-09-04 20:54:50 -0400

    Improve Makefile readability

--- common.mk
+++ common.mk
@@ -2,7 +2,7 @@ BENCHTIME ?= 3s
 
 .PHONY  : bench
 bench   :
-	@go test -run none -bench . -benchtime $(BENCHTIME) -benchmem
+	@go test -run none -bench . -benchtime ${BENCHTIME} -benchmem
 
 .PHONY  : test
 test    :
@@ -14,7 +14,7 @@ analysis:
 
 .PHONY  : help
 help    :
-	@echo "bench    : Runs benchmarks    (\044BENCHTIME=${BENCHTIME})"
-	@echo "test     : Runs tests"
-	@echo "analysis : Runs optimization analysis"
-	@echo "help     : This help"
+	@echo 'bench    : Runs benchmarks    ($$BENCHTIME=${BENCHTIME})'
+	@echo 'test     : Runs tests'
+	@echo 'analysis : Runs optimization analysis'
+	@echo 'help     : This help'
